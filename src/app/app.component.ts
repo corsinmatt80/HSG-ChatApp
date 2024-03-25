a009001bc922107fff5dda34600d202b4e384040
corsinmatt80 <corsin.matt@student.unisg.ch>
2024-03-25 17:39:02 +0100

    story 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ChatBarComponent } from './components/chat-bar/chat-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ChatBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/chat-bar/chat-bar.component.ts
@@ -0,0 +1,25 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-chat-bar',
+  standalone: true,
+  imports: [FormsModule, NgIf],
+  templateUrl: './chat-bar.component.html',
+  styleUrl: './chat-bar.component.css'
+})
+export class ChatBarComponent {
+public chatMessage="";
+public errorMessage = "";
+/**
+ * addMessage to ChatBox
+ */
+public addMessage(message: string): void {
+  if(message.length === 0){
+    this.errorMessage = "Bitte einen Nachricht einfügen!"
+  }
+  alert(message);
+}
+
+}
