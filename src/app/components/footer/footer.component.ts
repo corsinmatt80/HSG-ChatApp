4347e39c1527d3e4faf2d94241b907b60436d53a
Ferhat-94 <127219553+Ferhat-94@users.noreply.github.com>
2024-04-19 19:41:26 +0200

    Update footer.component.ts

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  public readonly currentYear = new Date().getFullYear();
 }
